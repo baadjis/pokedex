6de7e70cd8c9ce7a05d5cf92078e3dde2c18f688
baadjis <baadjisidy@gmail.com>
2019-11-19 14:13:46 +0100

    added errors hnadlers

--- back/pokedex/managers/abilities.py
+++ back/pokedex/managers/abilities.py
@@ -1,9 +1,13 @@
 import requests
 from playhouse.shortcuts import model_to_dict
 
-from pokedex.models.pokemon import Ability, Generation, AbilityEffects, VerboseEffect, Language,PokemonAbilities
+from pokedex.models.pokemon import Ability, Generation, AbilityEffects, VerboseEffect, Language, PokemonAbilities
+
 
 def load_ability_from_api(name):
+    """this function will load pokemon from api to the database
+    :parameter name the name of the pokemon"""
+
     request = requests.get(f'https://pokeapi.co/api/v2/ability/{name}')
     data = request.json()
 
@@ -23,7 +27,8 @@ def load_ability_from_api(name):
             language = Language.get_or_none(name=effect['language']['name'])
             if language is None:
                 language = Language.create(name=effect['language']['name'])
-            verbose_effect = VerboseEffect.create(effect=effect['effect'], short_effect=effect['short_effect'], language=language)
+            verbose_effect = VerboseEffect.create(effect=effect['effect'], short_effect=effect['short_effect'],
+                                                  language=language)
         ability_effect = AbilityEffects.create(ability=ability, effect=verbose_effect)
 
     return ability
@@ -46,31 +51,42 @@ def load_abilities_from_api():
         print(f'{i} abilities loaded.')
 
     return i
-def search_abilities(query=None,limits=None,offset=None):
-    if limits == None :
-        limits=10
-    if offset==None:
-        offset=0
-    abilities= (Ability.select(Ability.id,Ability.name,Ability.is_main_series,Ability.generation)
-                .join(Generation)).order_by(Ability.name).limit(limits).offset(offset)
-    if query!=None:
-         abilities=abilities.where(query in Ability.name)
+
+
+def search_abilities(query=None, limits=None, offset=None):
+    if limits == None:
+        limits = 10
+    if offset == None:
+        offset = 0
+    abilities = (Ability.select(Ability.id, Ability.name, Ability.is_main_series, Ability.generation)
+                 .join(Generation)).order_by(Ability.name).limit(limits).offset(offset)
+    if query != None:
+        abilities = abilities.where(query in Ability.name)
     if len(abilities) > (limits + offset):
-         abilities = abilities.limits(limits).offset(offset)
-    data=[dict({"id":ability.id,"name":ability.name,"is_main_series":ability.is_main_series,"generation":ability.generation.name}) for ability in abilities]
+        abilities = abilities.limits(limits).offset(offset)
+    data = [dict({"id": ability.id, "name": ability.name, "is_main_series": ability.is_main_series,
+                  "generation": ability.generation.name}) for ability in abilities]
     return data
-def get_ability_effects(abilityid):
+
+
+def get_ability_effects(ability_id):
+    """ get the  ability effect if given the ability id
+    :parameter abylity_id  :the ability id
+    """
     effects = (VerboseEffect.select(VerboseEffect.effect)
                .join(AbilityEffects)
                .join(Ability)
-               .where(Ability.id == abilityid))
+               .where(Ability.id == ability_id))
     return [ef.effect for ef in effects]
+
+
 def add_effects(liste):
-    for d in  liste:
-        d["effects"]=get_ability_effects(d["id"])
+    for d in liste:
+        d["effects"] = get_ability_effects(d["id"])
     return liste
+
 
 def filter_ability_by_generation(liste):
     genations = set([d["generation"] for d in liste])
-    filtered={gen:[d for d in liste if (gen == d["generation"])] for gen in genations}
+    filtered = {gen: [d for d in liste if (gen == d["generation"])] for gen in genations}
     return filtered
